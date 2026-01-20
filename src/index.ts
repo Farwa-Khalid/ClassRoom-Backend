ffa502f4bd9a93bc7abea0bb1fd704c740b37e19
Farwa Khalid <awanmahaz71@gmail.com>
2026-01-20 12:47:38 +0500

    implement all subjects

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 
-
 import express from 'express';
+import subjectsRouter from './routes/subject';
+import cors from "cors" ;
+
 const app = express();
 const PORT = 8000;
 
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello, welcome to the Classroom API!');
 });
+app.use('api/subjects',subjectsRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
